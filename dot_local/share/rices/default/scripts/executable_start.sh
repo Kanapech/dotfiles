a3be3dd9d66528889e3c520ca5f564634b4ed5e3
Kanapech <joran.meunier@gmail.com>
2026-04-01 11:34:35 +0200

    🔧 Updated ironbar config and added style

--- dot_local/share/rices/default/scripts/executable_start.sh
+++ dot_local/share/rices/default/scripts/executable_start.sh
@@ -3,9 +3,15 @@ pkill -x ironbar || true
 pkill -x swaync || true
 
 CONFIG="$HOME/.local/share/rices/default/ironbar/config.toml"
+THEME="$HOME/.local/share/rices/default/ironbar/style.css"
 
 if ! test -f "$CONFIG"; then
     echo "ERROR: inronbar config not found: $CONFIG"
+    exit 1
+fi
+
+if ! test -f "$THEME"; then
+    echo "ERROR: inronbar theme not found: $THEME"
     exit 1
 fi
 
