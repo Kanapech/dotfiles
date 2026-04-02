f0e2a75ab2f1c44eb8794091ce93aeef679f0f31
Kanapech <joran.meunier@gmail.com>
2026-04-02 20:02:08 +0200

    🔧 Made ironbar config loading as symlink instead of giving the file manually

--- dot_local/share/rices/default/scripts/executable_start.sh
+++ dot_local/share/rices/default/scripts/executable_start.sh
@@ -2,18 +2,5 @@
 pkill -x --wait ironbar || true
 pkill -x --wait swaync || true
 
-CONFIG="$HOME/.local/share/rices/default/ironbar/config.toml"
-THEME="$HOME/.local/share/rices/default/ironbar/style.css"
-
-if ! test -f "$CONFIG"; then
-    echo "ERROR: inronbar config not found: $CONFIG"
-    exit 1
-fi
-
-if ! test -f "$THEME"; then
-    echo "ERROR: inronbar theme not found: $THEME"
-    exit 1
-fi
-
-uwsm app -- ironbar -c "$CONFIG" -t "$THEME"
+uwsm app -- ironbar
 uwsm app -- swaync
